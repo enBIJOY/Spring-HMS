6b306614ac65efd4ec2514c64c9883a8ff5d6926
enBIJOY <mofizuldev@gmail.com>
2024-09-18 09:09:06 +0600

    Packeges, Class and Interface has been Created

--- src/main/java/com/hms/controller/HmsApplication.java
+++ src/main/java/com/hms/controller/HmsApplication.java
@@ -8,7 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HmsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test compilation for \"Bijoy\"");
-		//SpringApplication.run(HmsApplication.class, args);
+
+		SpringApplication.run(HmsApplication.class, args);
+		System.out.println("Test run for \"Spring HMS\"");
+
+		try {
+			// The newInstance() call is a work around for some broken Java implementations
+			Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+			//JOptionPane.showMessageDialog(null,"JDBC Driver load success");
+			System.out.println("JDBC Driver loaded successfully");
+		} catch (Exception ex) {
+			// handle the error
+			System.out.println("Problem with JDBC Driver"+ex.toString());
+			//JOptionPane.showMessageDialog(null,JOptionPane.ERROR_MESSAGE+"Problem with JDBC Driver");
+		}
 	}
 }
